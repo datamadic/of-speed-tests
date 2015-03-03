f739ba147905e1726350564f6d724795ce7ae34c
datamadic <xavier@openfin.co>
2015-03-03 16:34:02 -0500

    initial working version, needs cleanup

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-index.js
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    
+    MongoClient = require('mongodb').MongoClient,
+    url = 'mongodb://display:display@ds049171.mongolab.com:49171/heroku_app34521734',
+    Promise = require('es6-promise').Promise;
+
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname));
+
+app.get('/display.html', function (req, res) {
+  queryTimes()
+  	.then(function(results){
+	  	res.render('display', {results: results});
+	  },
+	  function(reason){
+	  	res.json({reason: reason});
+	  });
+});
+
+app.get('/data', function (req, res) {
+  queryTimes()
+  	.then(function(results){
+	  	res.render('display', {results: results});
+	  },
+	  function(reason){
+	  	res.json({reason: reason});
+	  });
+});
+
+server.listen(9000);
+
+function queryTimes() {
+		return new Promise(function(resolve, reject){
+
+			MongoClient.connect(url, function(err, db) {
+
+	        if (err) {
+	        	return reject(err);
+	        }
+
+	        var collection = db.collection('heroku_app34521734');
+	        collection.find().toArray(function(err, results) {
+
+	        	if (err) {
+		        	return reject(err);
+		        	
+		        }
+
+	          db.close();
+	          resolve(results);
+	        });
+	    });
+		});
+    
+}//end 
+
+//logToMongo();
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var express = require('express'),
     io = require('socket.io')(server),
     config = require('./config.js'),
     path = require('path'),
-    timing = {},
+    loggingInfo = {},
     spawn = require('child_process').spawn,
     child,
     fs = require('fs'),
-    moment = require('moment');
+    MongoClient = require('mongodb').MongoClient,
+    url = 'mongodb://speed:speed@ds049171.mongolab.com:49171/heroku_app34521734';
 
-console.log(config.rvmLocation, config.appConfig);
-//server.static('.');
+
 server.listen(9000);
 app.use(express.static(__dirname));
 
@@ -20,18 +20,20 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  //socket.emit('news', { hello: 'world' });
   socket.on('times', function (data) {
     
-    timing.appInitial = data.ofInitialTime;
-    timing.appLoaded = data.ofAfterLoaded;
-    //child.kill();
+    loggingInfo.appInitial = data.ofInitialTime;
+    loggingInfo.appLoaded = data.ofAfterLoaded;
+    loggingInfo.runtimeVersion = data.runtimeVersion;
+    loggingInfo.sysInfo = data.sysInfo;
     socket.emit('shutdown', 'true');
-    //console.log(timing);
+    getLogs();
+    logToMongo();
+    //console.log(data);
   });
 });
 
-timing.rvmInvoked = Date.now();
+loggingInfo.rvmInvoked = Date.now();
 
 child = spawn(config.rvmLocation, [ '--config', config.appConfig]);
 
@@ -41,30 +43,47 @@ child.on('error', function(err){
 
 child.on('close', function(){
   server.close();
-  console.log(path.dirname(config.rvmLocation));
-  getLogs();
+  // console.log(path.dirname(config.rvmLocation));
+  // getLogs();
+  // logToMongo();
 });
 
 function getLogs () {
-  var _rvmGrep = /\[ '(\d*\/\d*\/\d* \d*:\d*:\d*),(\d*)/,
-      rvmGrep = /(\d*\/\d*\/\d* \d*:\d*:\d*),(\d*)/,
+  var rvmGrep = /(\d*\/\d*\/\d* \d*:\d*:\d*),(\d*)/,
       rvmLog = fs.readFileSync(
     path.resolve(path.dirname(config.rvmLocation), 'logs', 'RVM.log'), {
     encoding: 'utf8'
   }).split('\n').slice(0,1);
-  //console.log(typeof rvmLog,rvmGrep.exec(rvmLog.toString()));
-  //console.log(moment.utc(rvmGrep.exec(rvmLog.toString())[1]));
-  timing.rvmInit = rvmGrep.exec(rvmLog.toString())[1];
 
+  loggingInfo.rvmInit = rvmGrep.exec(rvmLog.toString())[1];
+  console.log(rvmLog.toString(), loggingInfo.rvmInit);
 
-  var runtimeGrep = /\[ '\[(\d*\/\d*\/\d* \d*:\d*:\d*)/,
+  var runtimeGrep = /(\d*\/\d*\/\d* \d*:\d*:\d*)/,
       runtimeLog = fs.readFileSync(
-    path.resolve(path.dirname(config.rvmLocation), 'cache', config.runtimeVersion, 'desktop-'+ config.runtimeVersion, 'debug.log'), {
+    path.resolve(path.dirname(config.rvmLocation), 'cache', loggingInfo.runtimeVersion, 'desktop-'+ loggingInfo.runtimeVersion, 'debug.log'), {
     encoding: 'utf8'
   }).split('\n').slice(0,1);
-  console.log(runtimeGrep.exec(runtimeLog.toString()));
-  console.log(timing);
+  loggingInfo.runtimeInit = runtimeGrep.exec(runtimeLog.toString())[1];
+  //console.log(loggingInfo);
 }
+
+function logToMongo () {
+  MongoClient.connect(url, function(err, db) {
+    //assert.equal(null, err);
+    var test = db.collection('heroku_app34521734');
+
+    test.insert(loggingInfo, function(err, res){
+      console.log('yeah buddy', loggingInfo);
+    });
+
+    console.log("Connected correctly to server");
+
+    db.close();
+    process.exit();
+  });
+}
+
+//setInterval(function(){}, 5000);
 
 
 //C:\Users\openfin\AppData\Local\OpenFin\OpenFinRVM.exe --config="https://demoappdirectory.openf.in/desktop/config/apps/tmp/speed-tests/empty-app/3.0.2.16/app.json"
